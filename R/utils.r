11215787a696a62b0f799440469d8bf7a023ecbf
Yihui Xie <xie@yihui.name>
2015-03-29 00:00:00 +0000

    version 1.6

--- R/utils.r
+++ R/utils.r
@@ -1,15 +1,15 @@
 ## extract tags
-tag <- function(x) attr(x, "Rd_tag")
+tag = function(x) attr(x, "Rd_tag")
 
 ## replace tags
-untag <- function(x) {
+untag = function(x) {
   if (is.null(x)) return(NULL)
-  attr(x, "Rd_tag") <- "TEXT"
+  attr(x, "Rd_tag") = "TEXT"
   x
 }
 
 ## construct strings from rd
-reconstruct <- function(rd) {
+reconstruct = function(rd) {
   if (is.null(rd)) return()
 
   if (is.list(rd)) {
@@ -22,11 +22,11 @@ reconstruct <- function(rd) {
                      '}', sep = "", collapse = ""))
       } else if (length(rd) == 0) return(tag(rd))
     }
-    special <- tag(rd) == toupper(tag(rd))
-    singles <- tag(rd) %in% c('\\tab', '\\cr')
-    prefix <- ifelse(special, "",
+    special = tag(rd) == toupper(tag(rd))
+    singles = tag(rd) %in% c('\\tab', '\\cr')
+    prefix = ifelse(special, "",
                      paste(tag(rd), ifelse(singles, "", "{"), sep = ""))
-    suffix <- ifelse(special, "", ifelse(singles, "", "}"))
+    suffix = ifelse(special, "", ifelse(singles, "", "}"))
     paste(prefix, paste(sapply(rd, reconstruct), collapse = ""), suffix,
           sep = "")
   } else {
@@ -35,22 +35,67 @@ reconstruct <- function(rd) {
 }
 
 ## wrap strings with comment prefix
-comment_line <- function(x, exdent = 0) {
+comment_line = function(x, exdent = 0) {
   if (missing(x)) return(comment_prefix())
 
   strwrap(x, width = 80, exdent = exdent, prefix = comment_prefix())
 }
 
 ## add comments
-comment_tag <- function(tag, value) {
+comment_tag = function(tag, value) {
   if (is.null(value) || value == "" || length(value) == 0) return()
 
   comment_line(paste(tag, value), exdent = 0)
 }
 
 ## access the comment prefix
-comment_prefix <- function() {
+comment_prefix = function() {
   getOption("roxygen.comment", "#' ")
 }
 
 Rbin = function() shQuote(file.path(R.home('bin'), 'R'))
+
+tidy_examples = function(rd, idx0, idx1, ..., path) {
+  tmp = rd[idx0:idx1]
+  if (length(tmp) > 1 && tmp[2] == '# !formatR') {
+    rd = rd[-(idx0 + 1)]  # remove this token
+    return(rd)
+  }
+  tmp[1] = sub('^\\\\examples\\{', '', tmp[1])
+  nn = length(tmp)
+  tmp[nn] = sub('\\}$', '', tmp[nn])
+  txt = gsub('\\%', '%', tmp, fixed = TRUE) # will escape % later
+  txt = sub('^\\\\+dont(run|test|show)', 'tag_name_dont\\1 <- function() ', txt)
+  txt = tidy_code(txt, ...)
+  if (!inherits(txt, 'try-error')) {
+    txt = gsub('(^|[^\\])%', '\\1\\\\%', txt)
+    txt = gsub('tag_name_dont(run|test|show) <- function\\(\\) \\{', '\\\\dont\\1{', txt)
+    txt[txt == ''] = '\n'
+    txt = unlist(strsplit(txt, '\n'))
+    # remove the four spaces introduced by disguising \\dontrun as a function
+    if (length(idx2 <- grep('\\\\dont(run|test|show)\\{', txt))) {
+      for (i in idx2) {
+        j = i + 1
+        while (txt[j] != '}') {
+          txt[j] = sub('^    ', '', txt[j])
+          j = j + 1
+        }
+      }
+    }
+    txt = paste(txt, rep(c('\n', ''), c(length(txt) - 1, 1)), sep = '', collapse = '')
+    txt[1] = paste('\\examples{', txt[1], sep = '')
+    nn0 = length(txt)
+    txt[nn0] = paste(txt[nn0], '}', sep = '')
+    rd[idx0] = paste(txt, collapse = '\n')
+    if (idx1 > idx0) rd = rd[-((idx0 + 1):idx1)]
+  } else {
+    message('(!) failed to reformat examples code in ', path)
+    message(paste('   ', tmp, collapse = '\n'))
+  }
+  rd
+}
+
+in_dir = function(dir, expr) {
+  owd = setwd(dir); on.exit(setwd(owd))
+  expr
+}
--- inst/doc/Rd2roxygen.R
+++ inst/doc/Rd2roxygen.R
@@ -24,8 +24,10 @@ options(roxygen.comment = "##' ")
 str(info <- parse_file(rd.file))
 cat(create_roxygen(info), sep='\n') # parse_and_save() combines these two steps
 
-## ----rab,echo=FALSE-----------------------------------------------------------
-formatR::usage(rab)
+## ----rab, eval=FALSE, code=formatR::usage(rab)--------------------------------
+#  rab(pkg, build = TRUE, build.opts = "--no-manual", install = FALSE, install.opts = if (build) "" else "--with-keep.source", 
+#      check = FALSE, check.opts = "--as-cran --no-manual", remove.check = TRUE, reformat = TRUE, 
+#      before = NULL, ...)
 
 ## ----reformat-ex1, eval=FALSE, tidy=FALSE-------------------------------------
 #  ## original code
